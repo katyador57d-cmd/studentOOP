05c912ac8f637a12cbb0ae871822b7899d4fb92e
katyador57d-cmd <katyador57d@gmail.com>
2026-05-12 19:06:40 +0200

    Add Student class and Grade class

--- main.php
+++ main.php
@@ -1,1 +1,37 @@
+<?php
 declare(strict_types=1);
+
+require_once __DIR__ . '/src/Grade.php';
+require_once __DIR__ . '/src/Student.php';
+require_once __DIR__ . '/src/StudentList.php';
+require_once __DIR__ . '/src/Display.php';
+require_once __DIR__ . '/src/GradeFilter.php';
+require_once __DIR__ . '/src/MenuHandler.php';
+
+$grade = new Grade('Math', 'B', '12.03.2023');
+
+$grade = new Grade('Math', 'C', '12.03.2023');
+$grade = new Grade('Math', 'C', '12.03.2023');
+
+$grade = new Grade('English', 'F', '12.03.2023');
+$grade = new Grade('English', 'B', '12.03.2023');
+$grade = new Grade('English', 'F', '12.03.2023');
+$student = new Student('Kate', 21);
+$student->addGrade($grade);
+
+$newGrade = $student->getGradesBySubject('Math');
+//print_r($newGrade);
+
+$mostGrade = $student->getMostCommonGrade('English');
+//print_r($mostGrade);
+
+$bestGrades = $student->getBestGrade();
+//print_r($bestGrades);
+
+$s = $student->getSubjectMostCommonGrade('English');
+//print_r($s);
+
+$d = $student->hasGradeBySubject('Math', 'A');
+
+print_r($d);
+var_dump($d);
--- src/Student.php
+++ src/Student.php
@@ -2,5 +2,127 @@
 declare(strict_types=1);
 
 class Student {
+
+    private bool $isActive;
+    private array $grades;
+
+    public function __construct(
+        public readonly string $name,
+        public readonly int $age,
+    ) {
+        $this->isActive = true;
+        $this->grades = [];
+    }
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function desactivate(): void
+    {
+        $this->isActive = false;
+    }
+
+    public function addGrade(Grade $grade): void
+    {
+        $this->grades[] = $grade;
+    }
+
+    public function getGrades(): array 
+    {
+        return $this->grades;
+    }
+
+    public function getGradesBySubject(string $subject): array
+    {
+
+        $result = [];
+        foreach ($this->grades as $grade) {
+            if($grade->subject === $subject) {
+                $result[] = $grade;
+            }
+        }
+        return $result;
+    }
+
+    public function getMostCommonGrade(string $subject): ?string
+    {
+        if(empty($this->grades)) {
+            return null;
+        }
+
+        $gradesCount = [];
+
+        foreach($this->grades as $grade) {
+            if(!isset($gradesCount[$grade->grade])) {
+                $gradesCount[$grade->grade] = 0;
+            }
+            $gradesCount[$grade->grade]++;     
+        }
+
+        arsort($gradesCount);
+        return array_key_first($gradesCount);
+
+    }
+
+    public function getBestGrade(): ?string
+    {
+        if(empty($this->grades)) {
+            return null;
+        }     
+       
+        $bestGrade = $this->grades[0]->grade;
+
+        foreach($this->grades as $grade) {
+            if (array_search($grade->grade, Grade::GRADES) < array_search($bestGrade, Grade::GRADES)) {
+                $bestGrade = $grade->grade;
+            }
+        }
+
+        return $bestGrade;
+
+    }
+
+    public function getSubjectMostCommonGrade(string $subject): ?string
+    {  
+        $filtered= [];
+
+        foreach($this->grades as $grade) {
+            if($grade->subject === $subject) {
+                $filtered[] = $grade;
+            }
+        }
+
+        if(empty($filtered)) {
+            return null;
+        }
+        
+        $gradeCount = [];
+
+        foreach($filtered as $grade) {
+           
+            if(!isset($gradeCount[$grade->grade])) {
+                $gradeCount[$grade->grade] = 0;
+            }
+        $gradeCount[$grade->grade]++;
+        
+    }
+        
+        arsort($gradeCount);
+        return array_key_first($gradeCount);
+    }
+
+    public function hasGradeBySubject(string $subject, string $grade): bool
+    {
+        foreach($this->grades as $item) {
+            if($item->subject === $subject && $item->grade === $grade) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     
 }
