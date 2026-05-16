01dab385bea54d0826fc0b2d8accfa46de6cd213
katyador57d-cmd <katyador57d@gmail.com>
2026-05-16 16:57:05 +0200

    New changes in Student list and gradeFilter Classes

--- src/GradeFilter.php
+++ src/GradeFilter.php
@@ -2,5 +2,68 @@
 declare(strict_types=1);
 
 class GradeFilter {
-    
+
+    public function filterByActive(array $students, bool $activeOnly): array
+    {
+        $activeStudents = [];
+
+        foreach($students as $activeStudent) {
+            if ($activeOnly && $activeStudent->isActive()) {
+                $activeStudents[] = $activeStudent;
+            } elseif (!$activeOnly && !$activeStudent->isActive()) {
+                $activeStudents[] = $activeStudent;
+            }
+        }
+        return $activeStudents;
+    }
+
+    public function filterByBestGrade(array $students, string $grade): array
+    {
+        $result = [];
+        
+        foreach ($students as $student) {
+            if ($grade === $student->getBestGrade()) {
+                $result[] = $student;
+            }
+        }
+        return $result;
+    }
+
+    public function filterBySubjectGrade(array $students, string $subject, string $grade): array 
+    {
+        $result = [];
+
+        foreach ($students as $student) {
+            if ($subject === $student->hasGradeBySubject($subject, $grade)) {
+                $result[] = $student;
+            }
+        }
+        return $result;
+    }
+
+    public function sortByName(array $students): array
+    {
+        usort($students, fn($a, $b) => strcmp($a->name, $b->name));
+        return $students;
+    }
+
+    public function sortByBestGrade(array $students): array
+    {
+   
+        usort($students, function ($a, $b) {
+            $bestGradeA = $a->getBestGrade();
+            $bestGradeB = $b->getBestGrade();
+            
+            $indexA = array_search($bestGradeA, Grade::GRADES);
+            $indexB = array_search($bestGradeB, Grade::GRADES); 
+            return $indexA<=> $indexB;  
+        });
+        return $students;
+    }
+
+    public function sortByAge(array $students): array 
+    {
+        usort($students, fn ($a, $b) => $a->age <=> $b->age);
+        return $students;
+    }
 }
--- src/StudentList.php
+++ src/StudentList.php
@@ -2,5 +2,85 @@
 declare(strict_types=1);
 
 class StudentList {
+    private array $students;
+    public function __construct()
+    {    
+    $this->students = [];
+    }
+
+    public function add(Student $student): bool
+    {
+       if ($this->findByName($student->name) !== null) {
+        return false;
+       }
+
+       $this->students[] = $student; 
+       return true;
+    }
+
+    public function findByName(string $name): ?Student
+    {
+        foreach ($this->students as $student) {
+            if (strtolower($student->name) === strtolower($name)) {
+                return $student;
+            }
+        }
+        return null;
+    }
+
+    public function exists(string $name): bool
+    {
+        foreach($this->students as $student){
+            if ($student->name === $name) {
+                return true;
+        }
+        }
+        return false;
+    }
+
+    public function desactivateByName(string $name): bool
+    {
+        $student = $this->findByName($name);
+
+        if($student === null) {
+            return false;
+        }
+        
+        $student->desactivate();
+        return true;
+    }
+
+    public function getAll(): array
+    {
+        return $this->students;
+    }
     
+    public function isEmpty(): bool
+    {
+        if (empty($this->students)) {
+            return true;
+        } 
+        return false;
+    }
+
+    public function getBestStudent(): ?Student
+    {
+       $bestStudent = null;
+       $bestGrade = 'F';
+    
+       foreach ($this->students as $student) {
+        $grade = $student->getBestGrade();
+        
+        if ($grade === '') {
+            continue;
+        }
+
+        if (array_search($grade, Grade::GRADES) < array_search($bestGrade, Grade::GRADES )) {
+            $bestStudent = $student;
+            $bestGrade = $grade;
+        }
+    }
+        return $bestStudent;
+    }
+
 }
