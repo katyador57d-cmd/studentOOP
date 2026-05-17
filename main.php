c314806d976e07c9da46ec2c1d43ed98ee33be14
katyador57d-cmd <katyador57d@gmail.com>
2026-05-17 19:57:58 +0200

    tha last task is done

--- main.php
+++ main.php
@@ -8,34 +8,9 @@ require_once __DIR__ . '/src/Display.php';
 require_once __DIR__ . '/src/GradeFilter.php';
 require_once __DIR__ . '/src/MenuHandler.php';
 
-$student = new Student('Kate', 21);
-$student->addGrade(new Grade('Math', 'B', '2023-04-23'));
-$student->addGrade(new Grade('Math', 'C', '2023-04-23'));
-$student->addGrade(new Grade('English', 'F', '2021-04-23'));
-$student->addGrade(new Grade('English', 'B', '2024-04-23'));
-
-$newGrade = $student->getGradesBySubject('Math');
-//print_r($newGrade);
-
-$mostGrade = $student->getMostCommonGrade();
-//print_r($mostGrade);
-
-$bestGrades = $student->getBestGrade();
-//print_r($bestGrades);
-
-$s = $student->getSubjectMostCommonGrade('English');
-//print_r($s);
-
-$d = $student->hasGradeBySubject('Math', 'A');
-
-//print_r($d);
-//var_dump($d);
-
 $studentList = new StudentList();
-$studentList->add($student);
 $display = new Display();
 $filter = new GradeFilter();
-//$display->printSubjectList($studentList->getAll());
 
 $menu = new MenuHandler($studentList, $filter, $display);
 $menu->run();
